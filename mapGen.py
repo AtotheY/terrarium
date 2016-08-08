f5ed24b2dd2f6db263bc708899c4093b382331db
Anthony Sistilli <anthony@Anthonys-MacBook-Pro.local>
2016-08-07 20:47:23 -0500

    Finished initial version of the program.. results are very mixed

--- mapGen.py
+++ mapGen.py
@@ -1,7 +1,7 @@
 def mapGen(dimension):
   import numpy as np
   global_map =[[0 for x in range(dimension)] for y in range(dimension)]
-  for i in range (dimension):
+  for i in range (1,dimension):
       global_map[i][0] = 1
       global_map[0][i] = 1
       global_map[dimension-1][i] = 1
@@ -13,7 +13,7 @@ def mapGen(dimension):
       x = np.random.randint(1,dimension-1)
       y = np.random.randint(1,dimension-1)
       while y == x:
-          y = np.random.randint(0,dimension-1)
+          y = np.random.randint(1,dimension-1)
       global_map[i][x] = 1
       global_map[i][y] = 2
 
--- nn_terrarium.py
+++ nn_terrarium.py
@@ -1,25 +1,9 @@
 import numpy as np
 import mapGen as mg
+import copy
+import operator
+from collections import defaultdict
 
-dimension = 11
-mapData = mg.mapGen(dimension)
-global_map = mapData[0]
-pos_y = mapData[1][0]
-pos_x = mapData[1][1]
-
-
-scoreMap = []
-# Creates a map whos boarders are all covered by WATER
-# Example for 4 by 4 (dimension of 4)
-# 1 1 1 1
-# 1 0 0 1
-# 1 0 0 1
-# 1 1 1 1
-# Reccomended map is NO LESS than 11 by 11
-w1 = np.random.uniform(0,1)
-w2 = np.random.uniform(0,1)
-w3 = np.random.uniform(0,1)
-lastMove = ""
 def foodGen():
     x = np.random.randint(1,dimension-1)
     y = np.random.randint(1,dimension-1)
@@ -28,44 +12,29 @@ def foodGen():
         x = np.random.randint(1,dimension-1)
     global_map[x][y] = 2
 
-moves = 0
-hunger = 0
-max_moves = dimension * 5
-max_hunger = dimension * 2
-
-
-#printing the map properly
-# w = water, f = food, x for critter
-print "***** steps: ", moves, "/", max_moves, " ***** hunger: ", hunger, "/",max_hunger, " *****"
-for i in range (dimension):
-    for j in range (dimension):
-        if global_map[i][j]==1:
-            print "w",
-        elif global_map[i][j]==2:
-            print "f",
-        elif global_map[i][j]==9:
-            print "X",
-        else:
-            print " ",
-    print ""
 
 #Checking all the way left
 def checkLeft(tpos_y, tpos_x):
-    print tpos_y, tpos_x
+    #print tpos_y, tpos_x
     node1 = 0
     node2 = 0
     distance = 0
     for i in range (-tpos_x, 0):
+        die = False
         distance += 1
         #print "LEFT ITERATION: ", i , "IT IS A: ", global_map[tpos_y][-i-1]
         if global_map[tpos_y][-i-1] == 1:
             node1 = 0
             node2 = 1
+            if distance == 1:
+                die = True
             break
         elif global_map[tpos_y][-i-1] == 2:
             node1 = 1
             node2 = 1
             break
+    if die:
+        return node1, node2, -distance
     return node1, node2, distance
 
 #Checking all the way right
@@ -74,15 +43,20 @@ def checkRight(tpos_y, tpos_x):
     node2 = 0
     distance = 0
     for i in range (tpos_x + 1, dimension):
+        die = False
         distance += 1
         if global_map[tpos_y][i] == 1:
             node1 = 0
             node2 = 1
+            if distance == 1:
+                die = True
             break
         elif global_map[tpos_y][i] == 2:
             node1 = 1
             node2 = 1
             break
+    if die:
+        return node1, node2, -distance
     return node1, node2, distance
 
 #Checking all the way up
@@ -91,15 +65,20 @@ def checkUp(tpos_y, tpos_x):
     node2 = 0
     distance = 0
     for i in range (-tpos_y, 0):
+        die = False
         distance += 1
         if global_map[-i-1][tpos_x] == 1:
             node1 = 0
             node2 = 1
+            if distance == 1:
+                die = True
             break
         elif global_map[-i-1][tpos_x] == 2:
             node1 = 1
             node2 = 1
             break
+    if die:
+        return node1, node2, -distance
     return node1, node2, distance
 
 #Checking all the way down
@@ -108,15 +87,20 @@ def checkDown(tpos_y, tpos_x):
     node2 = 0
     distance = 0
     for i in range (tpos_y + 1, dimension):
+        die = False
         distance += 1
         if global_map[i][tpos_x] == 1:
             node1 = 0
             node2 = 1
+            if distance == 1:
+                die = True
             break
         elif global_map[i][tpos_x] == 2:
             node1 = 1
             node2 = 1
             break
+    if die:
+        return node1, node2, -distance
     return node1, node2, distance
 
 def neuralNet(arr):
@@ -128,8 +112,6 @@ def neuralNet(arr):
     initial1 = arr[0]
     initial2 = arr[1]
     distance = arr[2]
-    print w1, w2, w3
-    print initial1, initial2, distance
     return w1*initial1 + w2*initial2 + w3*(1/distance)
 
 
@@ -139,7 +121,7 @@ def getMove(tpos_y, tpos_x):
     up = neuralNet(checkUp(tpos_y, tpos_x))
     down = neuralNet(checkDown(tpos_y, tpos_x))
     myMax = max(left, right, up, down)
-    print left, right, up, down
+    #print left, right, up, down
     amount = ""
     if myMax == left:
         amount += "l"
@@ -149,24 +131,55 @@ def getMove(tpos_y, tpos_x):
         amount += "d"
     elif myMax == up:
         amount += "u"
-    print amount
+    #print amount
     return amount
 
+# ******************* PROGRAM ****************** #
+dimension = 11
+mapData = mg.mapGen(dimension)
 
+
+max_moves = dimension * 5
+max_hunger = dimension * 2
+global_map = mapData[0]
+
+
+scoreMap = {}
+
+
+# Creates a map whos boarders are all covered by WATER
+# Example for 4 by 4 (dimension of 4)
+# 1 1 1 1
+# 1 0 0 1
+# 1 0 0 1
+# 1 1 1 1
+# Recommended map is NO LESS than 11 by 11
+critterMap = defaultdict(list)
 
 for i in range (0,64):
-
-    score = 0
-    while True:
+    real_map = copy.deepcopy(mapData[0])
+    global_map = copy.deepcopy(real_map)
+    pos_y = copy.deepcopy(mapData[1][0])
+    pos_x = copy.deepcopy(mapData[1][1])
+    w1 = np.random.uniform(0,1)
+    w2 = np.random.uniform(0,1)
+    w3 = np.random.uniform(0,1)
+    critterMap[i].append([w1,w2,w3])
+    lastMove = ""
+    dead = False
+    moves = 0
+    hunger = 0
+    while not dead:
         command = getMove(pos_y, pos_x)
-        print "MOVING: ", command
+        #print "***** steps: ", moves, "/", max_moves, " ***** hunger: ", hunger, "/",max_hunger, " *****"
+        #print "MOVING: ", command
         moves += 1
         hunger += 1
         if moves == max_moves:
-            score = moves
+            scoreMap[i] = moves
             break
         if hunger == max_hunger:
-            score = moves
+            scoreMap[i] = moves
             break
         old_pos_y = pos_y
         old_pos_x = pos_x
@@ -178,10 +191,11 @@ for i in range (0,64):
             pos_x -= 1
         elif command == "r":
             pos_x += 1
-
+        else:
+            print ""
         if global_map[pos_y][pos_x] == 1:
-            score = moves
-            break
+            scoreMap[i] = moves
+            dead = True
         elif global_map[pos_y][pos_x] == 2:
             if hunger >= 5:
                 hunger -= 5
@@ -192,22 +206,149 @@ for i in range (0,64):
             global_map[old_pos_y][old_pos_x] = 0
 
         else:
-            print "new pos: ", pos_x, pos_y
             global_map[pos_y][pos_x] = 9
             global_map[old_pos_y][old_pos_x] = 0
+avg = 0
+for i in scoreMap:
+    avg += scoreMap[i]
+    #print i,scoreMap[i]
+print "Average for first epoch: ", avg/len(scoreMap)
+
+sorted_x = sorted(scoreMap.items(), key=operator.itemgetter(1))
+#print sorted_x[63]
+counterz = 0
+newCritterMap = defaultdict(list)
+repreductionList = []
+for i in range (-63,-56):
+    counterz += 1
+    #print " RANK #", counterz, " is critter : ", sorted_x[-i][0], " with a score of ", sorted_x[-i][1], " and weights of W1: " , critterMap[sorted_x[-i][0]][0][0], " W2: ", critterMap[sorted_x[-i][0]][0][1] , " W3: ", critterMap[sorted_x[-i][0]][0][2]
+    repreductionList.append(sorted_x[-i][0])
+#print "REP LIST: ", repreductionList
+
+countChildren = 0
+for q in range (len(repreductionList)):
+
+    parentA = repreductionList[q]
+    #print "first parent is: ",repreductionList[q], " with values ",  critterMap[sorted_x[parentA][0]][0][0],  critterMap[sorted_x[parentA][0]][0][1],  critterMap[sorted_x[parentA][0]][0][2]
+    for v in repreductionList:
+        parentB = v
+        #print "second parent is: ",parentB, " with values ",  critterMap[sorted_x[parentB][0]][0][0],  critterMap[sorted_x[parentB][0]][0][1],  critterMap[sorted_x[parentB][0]][0][2]
+        newCritterMap[countChildren].append([critterMap[sorted_x[parentA][0]][0][0],critterMap[sorted_x[parentB][0]][0][1],critterMap[sorted_x[parentA][0]][0][2]])
+        countChildren += 1
+
+for q in range (45,64):
+    w1 = np.random.uniform(0,1)
+    w2 = np.random.uniform(0,1)
+    w3 = np.random.uniform(0,1)
+    newCritterMap[q].append([w1,w2,w3])
+#print "DONE REP RODUCING. RESULTS: ", newCritterMap
+
+epoches = 100
+for e in range(1,epoches):
+    mapData = mg.mapGen(dimension)
 
 
+    max_moves = dimension * 5
+    max_hunger = dimension * 2
+    global_map = mapData[0]
+    for i in range (0,63):
+        real_map = copy.deepcopy(mapData[0])
+        global_map = copy.deepcopy(real_map)
+        pos_y = copy.deepcopy(mapData[1][0])
+        pos_x = copy.deepcopy(mapData[1][1])
+        w1 = newCritterMap[i][0][0]
+        w2 = np.random.uniform(0,1)
+        w3 = np.random.uniform(0,1)
+        critterMap[i].append([w1,w2,w3])
+        lastMove = ""
+        dead = False
+        moves = 0
+        hunger = 0
+        while not dead:
+            command = getMove(pos_y, pos_x)
+            #print "***** steps: ", moves, "/", max_moves, " ***** hunger: ", hunger, "/",max_hunger, " *****"
+            #print "MOVING: ", command
+            moves += 1
+            hunger += 1
+            if moves == max_moves:
+                scoreMap[i] = moves
+                break
+            if hunger == max_hunger:
+                scoreMap[i] = moves
+                break
+            old_pos_y = pos_y
+            old_pos_x = pos_x
+            if command == "d":
+                pos_y += 1
+            elif command == "u":
+                pos_y -= 1
+            elif command == "l":
+                pos_x -= 1
+            elif command == "r":
+                pos_x += 1
+            else:
+                print ""
+            if global_map[pos_y][pos_x] == 1:
+                scoreMap[i] = moves
+                dead = True
+            elif global_map[pos_y][pos_x] == 2:
+                if hunger >= 5:
+                    hunger -= 5
+                else:
+                    hunger = 0
+                foodGen()
+                global_map[pos_y][pos_x] = 9
+                global_map[old_pos_y][old_pos_x] = 0
+
+            else:
+                global_map[pos_y][pos_x] = 9
+                global_map[old_pos_y][old_pos_x] = 0
+    avg = 0
+    for i in scoreMap:
+        avg += scoreMap[i]
+        #print i,scoreMap[i]
+    print "Epoch ", e, " average lifespan is : ", avg/len(scoreMap), " steps per critter"
+
+    sorted_x = sorted(scoreMap.items(), key=operator.itemgetter(1))
+    #print sorted_x[63]
+    counterz = 0
+    newCritterMap = defaultdict(list)
+    repreductionList = []
+    for i in range (-63,-56):
+        counterz += 1
+        #print " RANK #", counterz, " is critter : ", sorted_x[-i][0], " with a score of ", sorted_x[-i][1], " and weights of W1: " , critterMap[sorted_x[-i][0]][0][0], " W2: ", critterMap[sorted_x[-i][0]][0][1] , " W3: ", critterMap[sorted_x[-i][0]][0][2]
+        repreductionList.append(sorted_x[-i][0])
+    #print "REP LIST: ", repreductionList
+
+    countChildren = 0
+    for q in range (len(repreductionList)):
+
+        parentA = repreductionList[q]
+        #print "first parent is: ",repreductionList[q], " with values ",  critterMap[sorted_x[parentA][0]][0][0],  critterMap[sorted_x[parentA][0]][0][1],  critterMap[sorted_x[parentA][0]][0][2]
+        for v in repreductionList:
+            parentB = v
+            #print "second parent is: ",parentB, " with values ",  critterMap[sorted_x[parentB][0]][0][0],  critterMap[sorted_x[parentB][0]][0][1],  critterMap[sorted_x[parentB][0]][0][2]
+            newCritterMap[countChildren].append([critterMap[sorted_x[parentA][0]][0][0],critterMap[sorted_x[parentB][0]][0][1],critterMap[sorted_x[parentA][0]][0][2]])
+            countChildren += 1
+
+    for q in range (45,64):
+        w1 = np.random.uniform(0,1)
+        w2 = np.random.uniform(0,1)
+        w3 = np.random.uniform(0,1)
+        newCritterMap[q].append([w1,w2,w3])
 
 #
 # print "***** steps: ", moves, "/", max_moves, " ***** hunger: ", hunger, "/",max_hunger, " *****"
-# for i in range (dimension):
-#     for j in range (dimension):
-#         if global_map[i][j]==1:
-#             print "w",
-#         elif global_map[i][j]==2:
-#             print "f",
-#         elif global_map[i][j]==9:
-#             print "X",
-#         else:
-#             print " ",
-#     print ""
+    #printing the map properly
+    # w = water, f = food, x for critter
+    #for v in range (dimension):
+    #    for j in range (dimension):
+    #        if global_map[v][j]==1:
+    #            print "w",
+    #        elif global_map[v][j]==2:
+    #            print "f",
+    #        elif global_map[v][j]==9:
+    #            print "X",
+    #        else:
+    #            print " ",
+    #    print ""
